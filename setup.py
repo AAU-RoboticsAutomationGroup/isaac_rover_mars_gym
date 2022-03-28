66d012c8aed2c5b978a9f1c0e00997419846b989
Anton <abmoRobotics@student.aau.dk>
2022-03-28 12:58:31 +0000

    Updated

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ INSTALL_REQUIRES = [
 
 # Installation operation
 setup(
-    name="RL",
+    name="isaacgymenvs",
     author="NVIDIA",
     version="0.1.0",
     description="Benchmark environments for high-speed robot learning in NVIDIA IsaacGym.",

00489b2d085b2905699bac79b51068b68ce15218
Anton <abmoRobotics@student.aau.dk>
2022-04-24 09:19:07 +0000

    New parameters

--- isaacgymenvs/learning/model.py
+++ isaacgymenvs/learning/model.py
@@ -9,7 +9,7 @@ class Conv(nn.Module):
         super(Conv,self).__init__()
         self.activation_functions = {
             "elu" : nn.ELU(),
-            "relu" : nn.ReLU(),
+            "relu" : nn.ReLU(inplace=True),
             "leakyrelu" :nn.LeakyReLU(),
             "sigmoid" : nn.Sigmoid(),
             "tanh" : nn.Tanh()
@@ -141,6 +141,40 @@ class StochasticActorHeightmap(GaussianModel):
             x = layer(x)
         return x, self.log_std_parameter
 
+
+class StochasticActorHeightmapGLU(GaussianModel):
+    def __init__(self, observation_space, action_space, num_exteroception=150, device = "cuda:0", network_features=[512,256,128], encoder_features=[80,60], activation_function="relu",clip_actions=False, clip_log_std = True, min_log_std= -20.0, max_log_std = 2.0):
+        super().__init__(observation_space, action_space, device, clip_actions)
+        self.num_exteroception = num_exteroception  # External information (Heightmap)
+        self.num_proprioception = observation_space.shape[0] - self.num_exteroception 
+        self.network = nn.ModuleList()  # MLP for network
+        self.encoder = nn.ModuleList()  # Encoder with MLPs for heightmap
+
+        # Create encoder for heightmap
+        in_channels = self.num_exteroception
+        for feature in encoder_features:
+            self.encoder.append(Conv(in_channels, feature, activation_function))
+            in_channels = feature
+        
+        # Create MLP
+        in_channels = self.num_proprioception + encoder_features[-1]
+        for feature in network_features:
+            self.network.append(Conv(in_channels, feature, activation_function))
+            in_channels = feature
+
+        self.network.append(nn.Linear(in_channels,action_space.shape[0]*2))
+        self.network.append(nn.GLU())
+        self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
+
+    def compute(self, states, taken_actions):
+        x = states[:,self.num_proprioception:]
+        for layer in self.encoder:
+            x = layer(x)
+        x = torch.cat((states[:,0:self.num_proprioception], x), dim=1)
+
+        for layer in self.network:
+            x = layer(x)
+        return x, self.log_std_parameter
 
 
 

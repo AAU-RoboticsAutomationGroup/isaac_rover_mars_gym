bb12516adff9dbf7ed0afd5e75ce998c8a4c3cee
abmoRobotics <abmo19@student.aau.dk>
2022-05-12 10:54:29 +0000

    added groups

--- isaacgymenvs/train_encoder.py
+++ isaacgymenvs/train_encoder.py
@@ -11,7 +11,7 @@ from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
 from skrl.trainers.torch import SequentialTrainer
 from skrl.envs.torch import wrap_env
 from skrl.envs.torch import load_isaacgym_env_preview2, load_isaacgym_env_preview3
-from learning.model import StochasticActor, StochasticCritic,StochasticActorHeightmap, DeterministicHeightmap, DeterministicHeightmapWithMemory, StochasticActorHeightmapWithMemory
+from learning.model import StochasticActor, StochasticCritic,StochasticActorHeightmap, DeterministicHeightmap, DeterministicHeightmapWithMemory, StochasticActorHeightmapWithMemory, DeterministicHeightmapWithCNN, StochasticActorHeightmapWithCNN
 from gym.spaces import Box
 from skrl.utils.model_instantiators import deterministic_model, Shape
 import math
@@ -39,6 +39,8 @@ models_ppo = {  "policy": StochasticActorHeightmap(env.observation_space, env.ac
                 "value": DeterministicHeightmap(env.observation_space, env.action_space, network_features=[256,160,128], encoder_features=[60,20] ,activation_function="leakyrelu")}
 # models_ppo = {  "policy": StochasticActorHeightmapWithMemory(env.observation_space, env.action_space, network_features=[256,160,128], encoder_features=[60,20], activation_function="leakyrelu"),
 #                 "value": DeterministicHeightmapWithMemory(env.observation_space, env.action_space, network_features=[256,160,128], encoder_features=[60,20] ,activation_function="leakyrelu")}
+#models_ppo = {  "policy": StochasticActorHeightmapWithCNN(env.observation_space, env.action_space, network_features=[256,160,128], encoder_features=[80,60], activation_function="leakyrelu"),
+#                "value": DeterministicHeightmapWithCNN(env.observation_space, env.action_space, network_features=[256,160,128], encoder_features=[80,60] ,activation_function="leakyrelu")}
 
 #https://machinelearningmastery.com/weight-initialization-for-deep-learning-neural-networks/
 # Initialize the models' parameters (weights and biases) using a Gaussian distribution
@@ -71,7 +73,8 @@ cfg_ppo["kl_threshold"] = 0.008
 # logging to TensorBoard and write checkpoints each 120 and 3000 timesteps respectively
 cfg_ppo["experiment"]["write_interval"] = 120
 cfg_ppo["experiment"]["checkpoint_interval"] = 3000
-cfg_ppo["experiment"]["experiment_name"] = "REMOVE3"
+cfg_ppo["experiment"]["experiment_name"] = "wandbTEST2"#"REMOVETestWithCNN-K5S2P2V3"
+cfg_ppo["experiment"]["group"] = "testGroup"#"REMOVETestWithCNN-K5S2P2V3"
 agent = PPO(models=models_ppo,
             memory=memory, 
             cfg=cfg_ppo, 
